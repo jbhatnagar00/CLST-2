fba37da54e4c088802ca07dc1855a65120ec50ad
jbhatnagar00 <37757414+jbhatnagar00@users.noreply.github.com>
2025-08-20 22:18:42 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { Amplify } from 'aws-amplify'
+import { cognitoUserPoolsTokenProvider } from 'aws-amplify/auth/cognito'
+import { sessionStorage } from 'aws-amplify/utils'
 import outputs from '../amplify_outputs.json'
 
+// Configure token persistence with cookies for better persistence
+cognitoUserPoolsTokenProvider.setKeyValueStorage(sessionStorage)
+
 // Configure Amplify with Gen 2 outputs
-Amplify.configure(outputs)
+Amplify.configure(outputs, {
+  ssr: false // Disable SSR for client-side app
+})
+
+console.log('Amplify configured with outputs:', outputs)
+
+// Test configuration immediately
+import { fetchAuthSession } from 'aws-amplify/auth'
+fetchAuthSession()
+  .then(session => {
+    console.log('Initial session check:', {
+      hasTokens: !!session.tokens,
+      hasCredentials: !!session.credentials
+    })
+  })
+  .catch(err => console.log('Initial session error:', err))
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
